79683d27dbf0573156634fd45ab9c1762297484f
EvoX7 <creativesoultouch@gmail.com>
2022-06-13 16:42:42 +0200

    Intro to JS

---
+++ js/main.js
@@ -0,0 +1,32 @@
+// Info user Name
+
+const userName = prompt("Insert your name");
+
+document.getElementById("user-name").innerHTML = userName;
+
+// Info user Surname
+
+const userSurname = prompt("Insert your Surname");
+
+document.getElementById("user-surname").innerHTML = userSurname;
+
+// Info user favourite color
+
+const favColor = prompt("Insert your favourite color");
+document.getElementById("user-fav-color").innerHTML = favColor;
+
+console.log(userName, userSurname, favColor);
+
+// Current Date to Add
+
+const currentYear = 22;
+
+
+// Password generator result 
+
+const pwGen = userName + userSurname + favColor + currentYear;
+document.getElementById("gen-pwd").innerHTML = pwGen;
+
+
+
+
